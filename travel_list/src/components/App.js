3b89a19fa1eb7a6615468f6defebd0572de75cc6
sophietao127 <sophietao127@gmail.com>
2024-05-03 17:12:57 -0700

    add packing list and items

--- travel_list/src/components/App.js
+++ travel_list/src/components/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
+import PackingList from "./PackingList";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -8,10 +9,37 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+    // console.log("toogle pass");
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
     </div>
   );
 }
