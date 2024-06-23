25f986a29604e8c28b4a69cf62c364a0c8a5e507
sophietao127 <sophietao127@gmail.com>
2024-06-23 16:25:50 -0700

    install native-stack

--- starting-project/App.js
+++ starting-project/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
